2fd54510136fdcd3138be8cd9a934574a889dabf
zohair <Zohair.ul.hasan@gmail.com>
2023-02-24 16:19:00 -0500

    added health endpoint to check if server is running

--- back/db_config.ts
+++ back/db_config.ts
@@ -1,9 +1,16 @@
+let port;
+if (process.env.DB_PORT) {
+    port = parseInt(process.env.DB_PORT);
+} else {
+    port = 5432;
+}
+
 let config = {
     user: process.env.DB_USER || "postgres",
     host: process.env.DB_HOST || "localhost",
     database: process.env.DB_NAME || "schedulerdb",
     password: process.env.DB_PASSWORD || "",
-    port: process.env.DB_PORT || 5432,
+    port,
 };
 
 export default config;
--- back/server.ts
+++ back/server.ts
@@ -7,9 +7,13 @@ app.use(express.json());
 
 app.use("/api/courses", coursesRouter);
 
+app.get("/health", (req, res) => {
+    res.send("OK");
+});
+
 let port = 3000;
 let host = "localhost";
 let protocol = "http";
 app.listen(port, host, () => {
-  console.log(`${protocol}://${host}:${port}`);
+    console.log(`${protocol}://${host}:${port}`);
 });
